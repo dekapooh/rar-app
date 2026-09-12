30882ca94a1a57042365e62b74e1dc5039b7372d
dekapooh <deepimpact.is.fly.to.next@gmail.com>
2026-09-13 05:14:23 +0900

    Cache separated Reference v1 fallback dataset

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME="rar-rc66-public-beta-v10-admin-direct";
+const CACHE_NAME="rar-reference-v1-data-v11";
 const APP_SHELL=[
-  "./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./hero-rc65.png","./brand-horse-rc65.png"
+  "./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./hero-rc65.png","./brand-horse-rc65.png","./data/rar-data-reference-v1.json"
 ];
 const RC66_BRAND_CSS=`
 /* RAR RC66 PUBLIC BETA FINAL */
