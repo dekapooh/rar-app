3bf37b561bafdec9a771f14820911aaf3f62ef07
dekapooh <deepimpact.is.fly.to.next@gmail.com>
2026-09-14 19:29:51 +0900

    Cache generated RAR v2 summaries

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME="rar-rc66-public-beta-v11-rar-v2-integration";
+const CACHE_NAME="rar-rc66-public-beta-v12-rar-v2-integration";
 const APP_SHELL=[
   "./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./hero-rc65.png","./brand-horse-rc65.png",
-  "./rar-v2-overlay.js?v=20260914-r2",
+  "./rar-v2-overlay.js?v=20260914-r3",
+  "./rar-v2-summaries.json",
   "./silk_2026_potential_beta49_recovered_v1.json",
   "./silk_2026_pedigree_v1.json",
   "./silk_2026_facts_v1.json"
