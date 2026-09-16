5d9447a20dc7cdc9a6114a4699c5974c5014b285
dekapooh <deepimpact.is.fly.to.next@gmail.com>
2026-09-17 07:49:41 +0900

    chore: refresh cache for fresh Builder pipeline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="rar-rc66-public-beta-v13-rar100-lastupdated";
+const CACHE_NAME="rar-rc66-public-beta-v14-fresh-builder-final";
 const APP_SHELL=[
   "./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./hero-rc65.png","./brand-horse-rc65.png"
 ];
