6a4756dfe5890646e5100917f1576507bf891195
dekapooh <deepimpact.is.fly.to.next@gmail.com>
2026-09-22 08:29:33 +0900

    chore: refresh PWA cache for final header theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="rar-rc66-public-beta-v16-header-meta-layout";
+const CACHE_NAME="rar-rc66-public-beta-v17-header-final-theme";
 const APP_SHELL=[
   "./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./hero-rc65.png","./brand-horse-rc65.png","./system-master-sync.js"
 ];
